436458b39c304b77030ea151b09062b2f4264885
marcosviniciusjau <marcosviniciusjahu@gmail.com>
2024-10-11 14:17:28 -0300

    first commit

---
+++ src/controllers/create-account.controller.ts
@@ -0,0 +1,50 @@
+import {
+  ConflictException,
+  Body,
+  Controller,
+  Post,
+  HttpCode,
+  UsePipes,
+} from '@nestjs/common'
+
+import { hash } from 'bcryptjs'
+import { ZodValidationPipe } from 'src/pipes/zod-validation-pipe'
+import { PrismaService } from 'src/prisma/prisma-service'
+import { z } from 'zod'
+
+const createAccountSchema = z.object({
+  name: z.string(),
+  email: z.string().email(),
+  password: z.string().min(6),
+})
+
+type CreateAccountSchema = z.infer<typeof createAccountSchema>
+@Controller('/accounts')
+export class CreateAccountController {
+  constructor(private prisma: PrismaService) {}
+  @Post()
+  @HttpCode(201)
+  @UsePipes(new ZodValidationPipe(createAccountSchema))
+  async handle(@Body() body: CreateAccountSchema) {
+    const { name, email, password } = body
+
+    const userExists = await this.prisma.user.findUnique({
+      where: {
+        email,
+      },
+    })
+
+    if (userExists) {
+      throw new ConflictException('User already exists')
+    }
+
+    const hashedPassword = await hash(password, 8)
+    await this.prisma.user.create({
+      data: {
+        name,
+        email,
+        password: hashedPassword,
+      },
+    })
+  }
+}
