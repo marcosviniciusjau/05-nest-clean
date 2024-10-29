be1dd34f6b200a23b504cf70e723dfcbe8570cf7
marcosviniciusjau <marcosviniciusjahu@gmail.com>
2024-10-29 14:38:56 -0300

    creating prisma repos

--- src/domain/forum/enterprise/entities/answer.ts
+++ src/domain/forum/enterprise/entities/answer.ts
@@ -10,7 +10,7 @@ interface AnswerProps {
   attachments: AnswerAttachmentList
   content: string
   createdAt: Date
-  updatedAt?: Date
+  updatedAt?: Date | null
 }
 
 export class Answer extends AggregateRoot<AnswerProps> {
